0d792fb3565f2e4052a93b7c3ff74c3ae35eb2bd
Canela <carpinteron@uninorte.edu.co>
2023-05-22 16:58:25 -0500

    j

--- src/iterativa.java
+++ src/iterativa.java
@@ -590,9 +590,7 @@ public class iterativa extends javax.swing.JFrame {
         res.setText(acum);
     }//GEN-LAST:event_jButton12ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
